81648c4297baeb2bb8a957c18e634de82c607860
Syawal Ludin <syawalludin2003@gmail.com>
2023-03-27 16:05:39 +0700

    feat: fix params for modalchat

--- src/pages/User/ModalChat.tsx
+++ src/pages/User/ModalChat.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router";
 import { useCookies } from "react-cookie";
 import axios from "axios";
@@ -10,30 +10,30 @@ import Input from "../../components/Input";
 import Button from "../../components/Button";
 
 interface ChatProps {
-  mentor_id: any;
-  student_id: any;
+  student_id: number;
+  mentor_id: number;
 }
 
-const ModalChat: React.FC<ChatProps> = ({mentor_id, student_id}) => {
+const ModalChat: React.FC<ChatProps> = ({ student_id, mentor_id }) => {
   const [message, setMessage] = useState<string>("");
   const [chats, setChats] = useState<ChatsType[]>([]);
-  const [loading, SetLoading] = useState(false);
+  const [loading, setLoading] = useState(false);
   const [cookie, setCookie] = useCookies(["token"]);
   const checkToken = cookie.token;
 
   useEffect(() => {
     ChatsList();
-  }, [mentor_id, student_id]);
+  }, []);
 
   function ChatsList() {
-    SetLoading(true);
-    axios.get(`/inbox`, {
+    setLoading(true);
+    axios.get(`/chats?mentor=${mentor_id}&student=${student_id}`, {
       headers: {
         Authorization: `Bearer ${checkToken}`
       }
     })
     .then((response) => {
-      const data = response.data.data;
+      const data = response.data?.data;
       setChats(data);
       console.log(data);
     })
@@ -41,12 +41,18 @@ const ModalChat: React.FC<ChatProps> = ({mentor_id, student_id}) => {
       console.log(error);
     })
   };
+  
 
-  const handleSendMessage = () => {
+  const handleNewChat = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setMessage(e.target.value)
+  }
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
     const body = {
-      mentor_id,
-      student_id,
-      message,
+      student_id: student_id,
+      mentor_id: mentor_id,
+      chats: message
     };
     axios.post(`/chats`, body, {
       headers: {
@@ -67,53 +73,38 @@ const ModalChat: React.FC<ChatProps> = ({mentor_id, student_id}) => {
     <div className="rounded-lg bg-white p-10">
       <div className="flex flex-col justify-center">
         <div className="card">
-          <div className="card-body">
-          {loading ? (
-            <p>No Message.</p>
-           ) : chats.map((items) => (
-             <>
-                <div className="mx-auto -mt-7">
-                  <p className="text-black text-3xl font-poppins">{items.sender_name}</p>
-                </div><div className="w-7/12 mt-11">
+          <div className="card-body over">
+            {chats.map((item, index) => (
+              <>
+                <div key={index} className="w-7/12">
                   <p className="text-black font-poppins font-semibold">
-                    {items.sender_name}
+                    {item.sender_name}
                   </p>
                   <div className="bg-white border border-black w-full h-14 flex justify-start items-center p-6 rounded-xl">
-                  <div className="text-black font-poppins">
-                  <span>{items.chat}</span>
+                    <div className="text-black font-poppins">
+                      <span>{item.chat}</span>
+                    </div>
                   </div>
                 </div>
-                      <div className="mt-6">
-                        <p className="text-black font-poppins font-semibold">
-                          {items.receiver_name}
-                        </p>
-                        <div className="bg-white border border-black w-full h-14 flex justify-start items-center p-6 rounded-xl">
-                          <div className="text-black font-poppins">
-                            <span>{items.chat}</span>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    </>
-                ))}
+              </>
+            ))}
           </div>
-          <form className="flex flex-row space-x-3">
-             <Input
+          <form onSubmit={handleSubmit} className="flex flex-row space-x-3">
+            <Input
               id="send"
               type="text"
               placeholder="Say something..."
-              onChange={(e: any) => setMessage(e.target.value)}
+              onChange={handleNewChat}
               value={message}
               className="w-full h-14 text-black font-poppins bg-white border border-black rounded-xl p-3"
-              />
-              <Button
+            />
+            <Button
               type="submit"
               label="Send"
               className="btn w-28 rounded-xl text-white"
-              onClick={handleSendMessage}
               disabled={!message.trim()}
-               />
-           </form>
+            />
+          </form>
         </div>
       </div>
     </div>
